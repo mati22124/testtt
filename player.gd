4899b2d765a522b0c79ef3e0c753f4754683b0e6
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-02 10:41:52 -0500

    fixed up/down

--- player.gd
+++ player.gd
@@ -3,7 +3,6 @@ extends KinematicBody2D
 var score : int = 0
 var pushtest : int = 1
 var speed : int = 200
-var speedy : int = 20
 var jumpForce : int = 600
 var gravity : int = 800
 var vel : Vector2 = Vector2()
@@ -11,12 +10,13 @@ onready var sprite : Sprite = get_node("Icon")
 
 func _physics_process(delta):
 	vel.x = 0
+	vel.y = 0
 	if Input.is_action_pressed("move_left"):
 		vel.x -= speed
 	if Input.is_action_pressed("move_right"):
 		vel.x += speed
 	if Input.is_action_pressed("move_up"):
-		vel.y -= speedy
+		vel.y -= speed
 	if Input.is_action_pressed("move_down"):
-		vel.y += speedy
+		vel.y += speed
 	vel = move_and_slide(vel, Vector2.UP)
