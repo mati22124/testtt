492a78d2ab5f7b44f30193ce229de06cd7e9ecb8
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-01 22:04:43 -0500

    first commit

---
+++ player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var score : int = 0
+var speed : int = 200
+var jumpForce : int = 600
+var gravity : int = 800
+var vel : Vector2 = Vector2()
+onready var sprite : Sprite = get_node("Icon")
+
+func _physics_process(delta):
+	vel.x = 0
+	if Input.is_action_pressed("move_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("move_right"):
+		vel.x += speed
+	vel = move_and_slide(vel, Vector2.UP)
