abf30e52a94b1ddaedaa07df06a38d9f0471df58
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-03 21:59:20 -0500

    Added floor and gravity

--- ctors/ctor.gd
+++ ctors/ctor.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
-class_name actor
+class_name ctor
 
+export var gravity: = 3000.0
+export var speed: = Vector2(300.0,1000.0)
+var velocity: =Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	
+	velocity.y += gravity*delta
+	velocity = move_and_slide(velocity)
