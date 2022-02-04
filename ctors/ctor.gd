f65a10717a49035dfdbd7e227cfbae0da5fc5268
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-04 11:26:10 -0500

    pp

--- ctors/ctor.gd
+++ ctors/ctor.gd
@@ -3,8 +3,9 @@ class_name ctor
 
 export var gravity: = 3000.0
 export var speed: = Vector2(300.0,1000.0)
-var velocity: =Vector2.ZERO
+var velocity: =Vector2(-300.0,0.0)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity*delta
 	velocity = move_and_slide(velocity)
+	
