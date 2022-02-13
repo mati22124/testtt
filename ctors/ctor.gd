bb03b918579a782c460fbcb0db679911d2ed9d3e
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-13 10:37:49 -0500

    forgot too commit lmfaoooooooooooooooooooooooooo

--- ctors/ctor.gd
+++ ctors/ctor.gd
@@ -3,7 +3,7 @@ class_name ctor
 
 export var gravity: = 3000.0
 export var speed: = Vector2(300.0,1000.0)
-var velocity: =Vector2(-300.0,0.0)
+var velocity: =Vector2(0.0,0.0)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity*delta
--- ctors/plyer.gd
+++ ctors/plyer.gd
@@ -1,1 +1,6 @@
 extends ctor
+
+func _physics_process(delta: float) -> void:
+	var direction:=Vector2(Input.get_action_strength("move_right")-Input.get_action_strength("move_left"),0.0)
+	velocity=speed*direction
+	velocity=move_and_slide(velocity)
