ef0ddf676792982762a05a57df2799b350c0c401
mati22124 <65964806+mati22124@users.noreply.github.com>
2022-02-15 15:49:04 -0500

    2/15 class

--- ctors/ctor.gd
+++ ctors/ctor.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 class_name ctor
 
-export var gravity: = 3000.0
-export var speed: = Vector2(300.0,1000.0)
-var velocity: =Vector2(0.0,0.0)
+const FLOOR_NORMAL:=Vector2.UP
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity*delta
-	velocity = move_and_slide(velocity)
+export var gravity: = 4000.0
+export var speed: = Vector2(300.0,1000.0)
+
+var velocity: =Vector2.ZERO
